e607b55b548d5b4907e6bd0525b69103482b6d76
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-29 22:12:25 +0300

    COMPLETED

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer"
+    ]
+
+    def __init__(self, name="", breed="Mutt"):
+        if name == "":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name
+            if breed not in Dog.APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+                breed = "Mutt"  # Assign default breed if not in the list
+            self.breed = breed
+            print(f"{self.name} is born! Breed: {self.breed}")
+
+
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,39 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = [
+        "Admin",
+        "Customer Service",
+        "Human Resources",
+        "ITC",
+        "Production",
+        "Legal",
+        "Finance",
+        "Sales",
+        "General Management",
+        "Research & Development",
+        "Marketing",
+        "Purchasing"
+    ]
+
+    def __init__(self, name="", job=""):
+        if name == "":
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(name, str):
+            print("Name must be string between 1 and 25 characters.")
+        elif len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self.name = name.title()
+            print(f"Person {self.name} is created.")
+
+        if job != "":
+            if job not in self.APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+                self.job = None  
+            else:
+                self.job = job
+                if hasattr(self, 'name') and hasattr(self, 'job'):
+                    print(f"{self.name} works as a {self.job}.")
+
+pass
